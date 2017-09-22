81d36cc719aec340257d88af6afc119a223046ce
Tom Hynes <thomas.hynes@outlook.com>
2017-09-22 15:47:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -13,3 +13,12 @@ function whileLoop(n)  {
   }
   return "done"
 }
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+ function doWhileLoop(array)  {
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue())
+}
